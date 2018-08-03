42e35da5d0585d4cb68edb4ee6ee72c8f3f2f843
mikespriggs26 <mikespriggs@msn.com>
2018-08-02 21:37:36 -0400

    Updates

---
+++ AdventureWorksAPI/CompanyResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventureWorksAPI
+{
+    public class CompanyResponse
+    {
+        public string symbol { get; set; }
+        public string companyName { get; set; }
+        public string exchange { get; set; }
+        public string industry { get; set; }
+        public string website { get; set; }
+        public string description { get; set; }
+        public string CEO { get; set; }
+        public string issueType { get; set; }
+        public string sector { get; set; }
+        public List<string> tags { get; set; }
+    }
+}
--- AdventureWorksAPI/Controllers/ValuesController.cs
+++ AdventureWorksAPI/Controllers/ValuesController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace AdventureWorksAPI.Controllers
 {
@@ -14,16 +16,50 @@ namespace AdventureWorksAPI.Controllers
 
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public string /*IEnumerable<string>*/ Get()
         {
-            return new string[] { "value1", "value2" };
+            var symbol = "unh";
+            var IEXTrading_API_PATH = "https://api.iextrading.com/1.0/stock/{0}/company";
+
+            IEXTrading_API_PATH = string.Format(IEXTrading_API_PATH, symbol);
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                //For IP-API
+                client.BaseAddress = new Uri(IEXTrading_API_PATH);
+                HttpResponseMessage response = client.GetAsync(IEXTrading_API_PATH).GetAwaiter().GetResult();
+                //string myResponse = response;
+
+                //if (response.IsSuccessStatusCode)
+                //{
+                    var historicalDataList = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                    
+                    string myResult = historicalDataList.ToString();
+               
+                    CompanyResponse item = Newtonsoft.Json.JsonConvert.DeserializeObject<CompanyResponse>(myResult);
+                
+                    string result = "Company name: " + item.companyName + "\nSymbol: " + item.symbol + "\nExchange: "
+                    + item.exchange + "\nIndustry: " + item.industry + "\nWebsite: " + item.website + "\nCEO: " +
+                    item.CEO + "\nIssue Type: " + item.issueType + "\nSector: " + item.sector + "\nDescription: " +
+                    item.description;  
+
+                //}
+
+                return result;
+                
+            }
+            
+            //return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "valueinde";
         }
 
         // POST api/values
